226a82aae53583d0bf9e887a0e1ca3bce7bf21bb
AllanJRY <jarry.allan@gmail.com>
2024-01-12 13:31:44 +0100

    feat: project init + linear search

---
+++ src/algo/linear_search.rs
@@ -0,0 +1,48 @@
+#[allow(clippy::manual_find)]
+pub fn linear_search<T: PartialEq>(haystack: &[T], needle: T) -> Option<&T> {
+    for val in haystack {
+        if *val == needle {
+            return Some(val);
+        }
+    }
+
+    None
+}
+
+pub fn linear_search_alt<T: PartialEq>(haystack: &[T], needle: T) -> Option<&T> {
+    haystack.iter().find(|&val| *val == needle)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_linear_search_i32() {
+        assert_eq!(Some(&3), linear_search(&[12, 45, 3, 2, 7, 68, 493, 90], 3));
+    }
+
+    #[test]
+    fn test_linear_search_string() {
+        assert_eq!(
+            Some(&"Ellie"),
+            linear_search(&["Emile", "Julie", "Ellie", "Joel", "Frank"], "Ellie")
+        );
+    }
+
+    #[test]
+    fn test_linear_search_alt_i32() {
+        assert_eq!(
+            Some(&3),
+            linear_search_alt(&[12, 45, 3, 2, 7, 68, 493, 90], 3)
+        );
+    }
+
+    #[test]
+    fn test_linear_search_alt_string() {
+        assert_eq!(
+            Some(&"Ellie"),
+            linear_search_alt(&["Emile", "Julie", "Ellie", "Joel", "Frank"], "Ellie")
+        );
+    }
+}
