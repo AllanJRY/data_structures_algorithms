4b11a5868a15583b9fdb21737001b1bb2d68477a
Allan <jarry.allan.dev@gmail.com>
2024-01-14 15:45:54 +0100

    feat(stuctures): mod init

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use algo::two_crystal_balls::two_crystal_balls;
-
 mod algo;
+mod structures;
 
 // TODO: make fn which use algos for solving the same problems, and do benchs
 
 fn main() {
-    two_crystal_balls(&[false, false, false, false, false, false, false, true, true]);
+    todo!();
 }
---
+++ src/structures/linked_list.rs
@@ -0,0 +1,2 @@
+// TODO: the implementation is complex in rust but is a good way to learn
+// basics of data structures. Should be done later.
